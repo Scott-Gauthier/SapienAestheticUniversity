99b0f54a493dc2a26957fceeb456b3fbad8b56aa
Scott-Gauthier <gauthier.scott@gmail.com>
2022-12-06 20:22:55 -0800

    Worked on the admin, creator, user setup so we could create those various profiles.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 require('dotenv').config();
 
 const connectionString =
@@ -7,17 +6,9 @@ const connectionString =
 
 // Wrap Mongoose around local connection to MongoDB
 mongoose.connect(connectionString, {
-=======
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
->>>>>>> a646a80f0954133fd21bc3cd3dbfb749d26fc4fa
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-<<<<<<< HEAD
 // Export connection
 module.exports = mongoose.connection;
-=======
-module.exports = mongoose.connection;
->>>>>>> a646a80f0954133fd21bc3cd3dbfb749d26fc4fa
