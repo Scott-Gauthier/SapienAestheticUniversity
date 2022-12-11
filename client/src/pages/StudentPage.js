bc939ea6679076495ef6853cc7a84f21e3edadd8
Mona Lisa <zeinabhared@gmail.com>
2022-12-10 17:59:32 -0800

    updated user login, creator login, and signup file pages

--- client/src/pages/StudentPage.js
+++ client/src/pages/StudentPage.js
@@ -4,7 +4,7 @@ function StudentPage(){
     // use hooks to get list of all classes with graphql or routes
     return(
         <main>
-            <h2>This is a page a student to see their classes</h2>
+            <h2>This is a page for a student to see their classes</h2>
         </main>
     )
 };
