59ef33b15ea2a8b44a838d2bd238301db314dc0c
Scott-Gauthier <gauthier.scott@gmail.com>
2022-12-11 19:49:32 -0800

    Reverted back to original hash code.

--- server/models/creator.js
+++ server/models/creator.js
@@ -26,6 +26,14 @@ const creatorSchema = new Schema({
 // }
 );
 
+creatorSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
 // custom method to compare and validate password for logging in
 creatorSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
