8403efd94d25534a4f917f0c975b9343d6a68bf5
Manuelacreate <107735377+Manuelacreate@users.noreply.github.com>
2022-12-13 19:45:11 -0800

    added assets

--- client/src/pages/ClassPage.js
+++ client/src/pages/ClassPage.js
@@ -6,6 +6,12 @@ function ClassPage(){
     return(
         <main>
             <h2>This is a page for all classes</h2>
+            <div style="position:relative;width:fit-content;height:fit-content;">
+            <a style="position:absolute;top:20px;right:1rem;opacity:0.8;" href="https://clipchamp.com/watch/7r5dzaWdCYA?utm_source=embed&utm_medium=embed&utm_campaign=watch">
+                <img style="height:22px;" src="https://clipchamp.com/e.svg" alt="Made with Clipchamp" />
+            </a>
+            <iframe allowfullscreen style="border:none" src="https://clipchamp.com/watch/7r5dzaWdCYA/embed" width="640" height="360"></iframe>
+        </div>"
         </main>
     )
 };
