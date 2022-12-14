f621e6fb436e5013d71ffe354fe7304141e84b5c
Scott-Gauthier <gauthier.scott@gmail.com>
2022-12-13 22:01:12 -0800

    Seed admin, user, creator password now hashes.

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 const Admin = require('./Admin');
+const Content = require('./Content');
 const Creator = require('./Creator');
 const User = require('./User');
 
-module.exports = { Admin, Creator, User };
+
+module.exports = { Admin, Content, Creator, User };
--- server/seeds/creator-seeds.js
+++ server/seeds/creator-seeds.js
@@ -2,18 +2,13 @@ const { Creator } = require('../models');
 
 const seedCreator = async () => {
 
-    await Creator.deleteMany({});
+  await Creator.deleteMany({});
 
-const data =  await Creator.insertMany(
-      [
-        { creatorname: 'Bob', email: 'bob@bob.com', password: 'password12345' }, //, thoughts: [1,5], friends: [2,3,4]
-        { creatorname: 'Dan', email: 'Dan@Dan.com', password: 'password12345' }, //, thoughts: [2, 6], friends: [1,3,4]
-        { creatorname: 'Lyndsey', email: 'Lyndsey@Lyndsey.com', password: 'password12345' }, //, thoughts: 3, friends: [1,2,4]
-        { creatorname: 'Scott', email: 'Scott@Scott.com', password: 'password12345' }, //, thoughts: 4, friends: [1,2,3] 
-      ],
-    ).then((err) =>
+  const data = await Creator.create(
+    { creatorname: 'Dan', email: 'Dan@Dan.com', password: 'password12345' },
+
+  ).then((err) =>
     err ? console.log(err) : console.log('Inserted'));
-
 }
 
-module.exports = {seedCreator};
+module.exports = { seedCreator };
