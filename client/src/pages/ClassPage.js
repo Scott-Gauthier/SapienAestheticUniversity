224afffd7cd142520a974b9decfd2c4ef1281418
Lynchk07 <lynch.kristinj@gmail.com>
2022-12-14 19:42:12 -0800

    commented out Classpage code

--- client/src/pages/ClassPage.js
+++ client/src/pages/ClassPage.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
-import ReactPlayer from 'react-player'
-import ClassCard from "../components/ClassCard";
+// import ReactPlayer from 'react-player'
+// import ClassCard from "../components/ClassCard";
 
 function ClassPage(){
     // use hooks to get list of all classes with graphql or routes
@@ -8,7 +8,7 @@ function ClassPage(){
         <main>
             <h2>This is a page for all classes</h2>
 
-            <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+            {/* <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' /> */}
             
         </main>
     )
