13d6cb3df56b8d6e013ff9b1eb2939672f2b64f4
Mona Lisa <zeinabhared@gmail.com>
2022-12-16 20:01:35 -0800

    updated app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ const client = new ApolloClient({
 
 function App() {
   return (
-  <ApolloProvider  client ={client}>
+  <ApolloProvider client ={client}>
    <Router>
         <AppNavbar/>
         <HeroCarousel/>
