1f3f049af8637dad7146c3b46579e6c47bbc6842
Lynchk07 <lynch.kristinj@gmail.com>
2022-12-16 20:20:34 -0800

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,26 @@ import Footer from './components/Footer';
 import ClassPage from './pages/ClassPage';
 import TeamPage from './pages/TeamPage';
 import StudentPage from './pages/StudentPage';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { Link } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
