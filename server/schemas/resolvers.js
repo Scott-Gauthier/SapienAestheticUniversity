376a2ab7f70d1a38e545f16280657429ec8f1b00
Manuelacreate <107735377+Manuelacreate@users.noreply.github.com>
2022-12-17 18:24:00 -0800

    version change

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const { signToken } = require(`../utils/auth`);
 const { AuthenticationError } = require(`apollo-server-express`);
 const stripe = require('stripe'); 
 
+
 const resolvers = {
     Query: {
         User: async(parent, args, context) => {
@@ -86,6 +87,6 @@ const resolvers = {
     //     throw new AuthenticationError(`The user must log in`)
     //     }
     }
-}
+
 
 module.exports = resolvers;
