c22d80918d1f19916482d7b5c4356cfea78e8edf
Mona Lisa <zeinabhared@gmail.com>
2022-12-18 20:15:08 -0800

    error fixed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,83 +1,31 @@
-const { User, Content, Order } = require(`../models`);
+const { User, Content } = require(`../models`);
 const { signToken } = require(`../utils/auth`);
 const { AuthenticationError } = require(`apollo-server-express`);
-//const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc'); 
+const stripe = require('stripe'); 
+
 
 const resolvers = {
     Query: {
-        User: async(parent, args, context) => {
+        getUser: async(parent, args, context) => {
             if (context.user) {
-                const user = await User.findById(context.user._id).populate({
-                    path: 'orders.orderedContent',
-                    populate: 'title'
-                });
-                return user
-            }
-            throw new AuthenticationError('Not logged in');
-        },
-
-        Content: async(parent, { title }) => {
-            const params = {};
-            if (title) {
-                params.title = title;
-            }
-            return await Content.findById(params).populate('title');
-        },
-        
-        AllContent: async(parent, args, context) => {
-            // const params = {};
-            // if (title) {
-            //     params.title = title;
-            // }
-            return await Content.find();
-        },
-
-        order: async (parent, { _id }, context) => {
-            if (context.user) {
-                const user = await User.findById(context.user._id).populate({
-                    path: 'orders.orderedContent',
-                    populate: 'title'
-                });
-                return user.orders.id(_id);
+                const userData = await User.findOne({ _id: context.user._id })
+                .select(`-_v -password`)
+                return userData;
             }
             throw new AuthenticationError(`The user is not logged in`);
         },
-        checkout: async (parent, args, context) => {
-            const url = new URL(context.headers.referer).origin;
-            const order = new Order({ orderedContent: args.content });
-            const line_items = [];
-      
-            const { orderedContent } = await order.populate('orderedContent');
-      
-            for (let i = 0; i < orderedContent.length; i++) {
-                const content = await stripe.orderedContent.create({
-                    name: orderedContent[i].name,
-                    description: orderedContent[i].description,
-                    images: [`${url}/images/${orderedContent[i].image}`]
-                });
-      
-                const price = await stripe.prices.create({
-                    content: content.id,
-                    unit_amount: [i].price * 100,
-                    currency: 'usd',
-                });
-      
-                line_items.push({
-                    price: price.id,
-                    quantity: 1
-                });
-            }
-      
-            const session = await stripe.checkout.sessions.create({
-                payment_method_types: ['card'],
-                line_items,
-                mode: 'payment',
-                success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-                cancel_url: `${url}/`
-            });
-      
-            return { session: session.id };
-        }           
+
+        // Content: async(parent, { title }) => {
+        //     const params = {};
+        //     if (title) {
+        //         params.title = title;
+        //     }
+        //     return await Content.findById(params).populate('title');
+        // },
+        
+        AllContent: async () => {
+            return Content.find();
+        }
     },
 
     Mutation: {
@@ -113,27 +61,26 @@ const resolvers = {
         
     // saveContent: async (parent, args , context) => {
     //     if (context.user) {
-    //         const updatedUser = await User.findOneAndUpdate(
+    //         const updatedContent = await User.findOneAndUpdate(
     //             { _id: context.user_id },
-    //             { $push: { studentcontent: args._id }},
+    //             { $addToSet: { studentcontent: args._id }},
     //             { new: true }
     //         )
-    //         return updatedUser;
+    //         return updatedContent;
     //     }
     // },
         
     // removeContent: async (parent, args, context) => {
     //     if (context.content) {
-    //         const updatedUser = await User.findOneAndUpdate(
+    //         const updatedContent = await User.findOneAndUpdate(
     //             { _id: context.user_id },
-    //             { $ToSet: { studentcontent: args.content}},
-    //             { new: true }                    
+    //             { $pull: { studentcontent: args._id }}                 
     //         );
     //         return updatedContent;
     //     }
     //     throw new AuthenticationError(`The user must log in`)
     //     }
-    //}
-}
+    }
+
 
 module.exports = resolvers;
