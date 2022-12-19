41f57c93a08c314c105747c86ddaf83394f93830
Scott-Gauthier <gauthier.scott@gmail.com>
2022-12-18 16:10:05 -0800

    Stash

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
-import { useQuery } from "@apollo/client";
-import { QUERY_ALL_CONTENT } from '../utils/Queries';
-import { saveContent, deleteBook } from '../utils/API';
 import HeroCarousel from '../components/HeroCarousel';
 import Student from './Student'
 
@@ -11,31 +7,8 @@ function Home() {
   
   return (
     <div>
-       <HeroCarousel/>
-
-      {data.AllContent.map((element) => {
-        console.log(element)
-        
-      return (
-        <Row xs={1} md={3} className="g-4 py-3">
-
-          <Col>
-            <Card key={element.id}>
-              <Card.Img variant="top" src={require("../assets/SpacePicsForArticles/space"+element.image+".png")}/>
-              <Card.Body>
-                <Card.Title>{element.title}</Card.Title>
-                <Card.Text>
-                
-                  {element.description}
-
-                </Card.Text>
-                <SaveButton onClick={() => handleSaveContent(element.id)}/>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row> 
-        )}
-      )}
+      <HeroCarousel/>
+      <Student />
     </div>
   )
 }
